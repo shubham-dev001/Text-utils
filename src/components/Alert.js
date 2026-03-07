d98fa168cc7ef6e48b620e12056032be92496c45
shubham vishwakarma <shubhamkumar911975@gmail.com>
2026-03-07 16:30:57 +0530

    update

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -14,22 +14,4 @@ export default function Alert(props) {
 }
 
 
-// export default function Alert({ alert }) {
-
-//   if (!alert) return null;
-
-//   return (
-//     <div className="container"  style={{height: "50px"}}>
-//       <div className="alert alert-warning alert-dismissible fade show" role="alert">
-//         <strong>{alert.type}</strong> : {alert.msg}
-//         <button 
-//           type="button" 
-//           className="btn-close" 
-//           data-bs-dismiss="alert" 
-//           aria-label="Close">
-//         </button>
-//       </div>
-//     </div>
-//   )
-// }
 
